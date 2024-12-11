7f19ef4151cdb0f6c67ef6a22082e9ce2569e4b6
Daf <afahd.ali@gmail.com>
2024-12-11 16:03:52 +0100

    feat: scaffold project

---
+++ src/Daf.MongoEf.Domain/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson;
+
+namespace Daf.MongoEf.Domain.Models;
+
+public class Customer
+{
+    public ObjectId Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Order { get; set; } = string.Empty;
+}
---
+++ src/Daf.MongoEf.Domain/Store/IGetCustomer.cs
@@ -0,0 +1,9 @@
+﻿using Daf.MongoEf.Domain.Models;
+
+namespace Daf.MongoEf.Domain.Store;
+
+public interface IGetCustomer
+{
+    public Customer? GetCustomer(int customerId);
+    public IEnumerable<Customer> GetAll();
+}
